ec0b05059c21f25a323f60d0e30bcbcbded45f6e
unknown <you@example.com>
2016-12-27 10:58:30 +0800

    gh-pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App.js';
-import Routes from './Routes.js';
-
-ReactDOM.render(<Routes />,document.getElementById('root'));
